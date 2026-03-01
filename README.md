b20c9ebc9599deaa114833497f9966cc128ed6cc
jsnshrmn <jsnshrmn@users.noreply.github.com>
2026-02-28 20:42:41 -0600

    fork: remove backlight-related features not needed for DeckSight

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Dimmer Deck
+# Dimmer DeckSight
 
-Make your Steam Deck even dimmer.
+Adjust DeckSight brightness with default controls. Derived from [DimmerDeck](https://github.com/chenx-dust/DimmerDeck). Made for [DeckSight](https://www.shadetechnik.com/decksight-install). Not affiliated with either.
+
+Just install the plugin and dial in the brightness just like you would on your SteamDeck LCD before you installed the DeckSight kit.
 
 ## How does it work?
 
-Steam OS uses [Gamescope](https://github.com/ValveSoftware/gamescope) to composite the screen. Its color management can load custom color LUT. This plugin creates a custom LUT that makes the screen even dimmer than the blacklight can.
+Steam OS uses [Gamescope](https://github.com/ValveSoftware/gamescope) to composite the screen. Its color management can load custom color LUT. This plugin creates a custom LUT that makes the screen dimmer.
 
 ## Known issues
 
 - Other color management features, such as color temperature, saturation tuning and night mode, will be broken when the dimmer brightness is set, because custom LUT will replace these effects.
 - The screen may flicker when the brightness is set, especially when sliding the brightness slider, because Gamescope doesn't has a mutex to keep the consistency of custom LUT.
+- This will also impact external displays.
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,17 @@
 import {
-  PanelSection,
-  PanelSectionRow,
   staticClasses,
-  SliderField,
-  ToggleField,
   Unregisterable,
 } from "@decky/ui";
 import {
   callable,
   definePlugin,
-  toaster,
 } from "@decky/api"
-import { useState, useEffect } from "react";
-import { LuSun, LuSunMoon } from "react-icons/lu";
-import { localizationManager, L } from "./i18n";
-import { t } from 'i18next';
+import { LuSunMoon } from "react-icons/lu";
+import { localizationManager } from "./i18n";
 
-const DIMMER_BRIGHTNESS_DELTA = 0.05;
-const TOAST_TIMEOUT = 1000;
+const BRIGHTNESS_DELTA = 0.01;
 
-let lastSystemBrightness = 0.;
-let lastDimmerBrightness = 1.;
-let pluginChangedBrightness = false;
-
-let setUiBrightnessCallback = (_: number) => { };
-
+let lastBrightness = 1.;
 let unregisterBrightnessCallback: Unregisterable | null = null;
 
 const backendSetBrightness = callable<[number], void>("set_brightness");
@@ -35,110 +22,31 @@ const clampBrightness = (value: number) => Math.min(Math.max(value, 0.), 1.);
 
 const setBrightness = (value: number) => {
   value = clampBrightness(value);
-  console.log(`Setting dimmer brightness to ${value}`);
-  lastDimmerBrightness = value;
-  setUiBrightnessCallback(value);
-  if (value == 1.)
-    backendReset();
-  else
-    backendSetBrightness(value);
+  console.log(`Setting brightness to ${value}`);
+  lastBrightness = value;
+  backendSetBrightness(value);
 };
 
 const onBrightnessChangedCallback = (value: { flBrightness: number; }) => {
   const brightness = value.flBrightness;
   console.log(`Brightness changed to ${brightness}`);
-  if (lastSystemBrightness <= 0.) {
-    if (brightness > 0.) {
-      // increase dimmer brightness
-      if (lastDimmerBrightness !== 1.) {
-        window.SteamClient.System.Display.SetBrightness(0.);
-        pluginChangedBrightness = true;
-        setBrightness(lastDimmerBrightness + DIMMER_BRIGHTNESS_DELTA);
-        if (lastDimmerBrightness === 1.) {
-          toaster.toast({
-            title: "Dimmer Brightness",
-            body: "OFF",
-            icon: <LuSun />,
-            duration: TOAST_TIMEOUT,
-            expiration: TOAST_TIMEOUT,
-          });
-        }
-      }
-    } else {
-      // decrease dimmer brightness
-      if (pluginChangedBrightness) {
-        pluginChangedBrightness = false;
-        lastSystemBrightness = brightness;
-        return;
-      }
-      if (lastDimmerBrightness === 1.) {
-        toaster.toast({
-          title: "Dimmer Brightness",
-          body: "ON",
-          icon: <LuSunMoon />,
-          duration: TOAST_TIMEOUT,
-          expiration: TOAST_TIMEOUT,
-        });
-      }
-      setBrightness(lastDimmerBrightness - DIMMER_BRIGHTNESS_DELTA);
-    }
-  }
-  lastSystemBrightness = brightness;
+  lastBrightness = brightness;
+  setBrightness(lastBrightness + BRIGHTNESS_DELTA);
 }
 const setShortcutControl = (enabled: boolean) => {
   if (enabled && unregisterBrightnessCallback === null)
     unregisterBrightnessCallback =
       window.SteamClient.System.Display.RegisterForBrightnessChanges(onBrightnessChangedCallback);
   else if (!enabled && unregisterBrightnessCallback !== null) {
+    backendReset();
     unregisterBrightnessCallback.unregister();
     unregisterBrightnessCallback = null;
   }
 }
 
 function Content() {
-  const [uiBrightness, setUiBrightness] = useState(lastDimmerBrightness);
-  const [uiShortcutControl, setUiShortcutControl] = useState(
-    localStorage.getItem("dimmer_deck.shortcut") === "true"
-  );
-
-  useEffect(() => {
-    setUiBrightnessCallback = (value) => {
-      setUiBrightness(value);
-    };
-    return () => {
-      setUiBrightnessCallback = (_: number) => { };
-    };
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("dimmer_deck.shortcut", uiShortcutControl ? "true" : "false");
-    setShortcutControl(uiShortcutControl);
-  }, [uiShortcutControl]);
-
   return (
     <>
-      <PanelSection title={t(L.BRIGHTNESS)}>
-        <PanelSectionRow>
-          <SliderField
-            label={t(L.DIMMER_BRIGHTNESS)}
-            value={uiBrightness}
-            step={0}
-            max={1}
-            min={0}
-            onChange={setBrightness}
-          />
-        </PanelSectionRow>
-      </PanelSection>
-      <PanelSection title={t(L.CONTROL)}>
-        <PanelSectionRow>
-          <ToggleField
-            label={t(L.ENABLE_SHORTCUT_CONTROL)}
-            description={t(L.ENABLE_SHORTCUT_CONTROL_DESC)}
-            checked={uiShortcutControl}
-            onChange={setUiShortcutControl}
-          />
-        </PanelSectionRow>
-      </PanelSection>
     </>
   );
 };
@@ -146,13 +54,13 @@ function Content() {
 export default definePlugin(() => {
   localizationManager.init();
   backendActivate();
-  setShortcutControl(localStorage.getItem("dimmer_deck.shortcut") === "true");
+  setShortcutControl(true);
 
   return {
     // The name shown in various decky menus
-    name: "Dimmer Deck",
+    name: "Dimmer DeckSight",
     // The element displayed at the top of your plugin's menu
-    titleView: <div className={staticClasses.Title}>Dimmer Deck</div>,
+    titleView: <div className={staticClasses.Title}>Dimmer DeckSight</div>,
     // The content of your plugin's menu
     content: <Content />,
     // The icon displayed in the plugin list
